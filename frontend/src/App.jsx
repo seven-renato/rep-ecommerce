386f558c7b9051ad1d21f9c1c2592af22826a6b5
seven-renato <seven.renato@hotmail.com>
2023-06-27 22:56:18 -0300

    login and reset-password added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Login } from "./pages/Login";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
- 
+import { ResetPassword } from "./pages/ResetPassword";
+
 function App() {
   return (
 <BrowserRouter>
       <Routes>
 
         <Route path="/login" element={<Login />}/>
-        {/* <Route path="/register" element={<Register />}/>
-        <Route path="/reset-password" element={<ResetPassword />}/> */}
+        {/* <Route path="/register" element={<Register />}/> */}
+        <Route path="/reset-password" element={<ResetPassword />}/>
 
 
       </Routes>
