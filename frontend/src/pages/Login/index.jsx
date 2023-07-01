68b5d59e4d2f6df3e2c9ac43cc08f79f84397c04
seven-renato <seven.renato@hotmail.com>
2023-06-30 23:29:02 -0300

    handle login function

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -1,18 +1,34 @@
 import "./styles.scss"
 import { Link, useNavigate } from "react-router-dom"
-import { useState } from "react"
+import React, { useState } from "react"
 import { login } from "../../axios/api-calls"
+import { useDispatch, useSelector } from "react-redux";
+import { setMsg, resetMsg, setSuccess} from "../../redux/userRedux";
 
 export function Login(){
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch()
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
+
     const handleLogin = async (e) => {
         e.preventDefault()
         const validate = await login({"email": email, "password": password})
         if (validate) {
+            console.log(user)
+            dispatch(setSuccess(true));
             navigate("/profile")
+        } else {
+            console.log(user)
+            dispatch(setMsg("E-mail e/ou senha inválidos!"));
+            setTimeout(() => { dispatch(resetMsg()) }, 5000)
+            restartLogin()
         }
+    }
+    const restartLogin = () => {
+        setEmail("")
+        setPassword("")
     }
 
     return(
@@ -21,6 +37,13 @@ export function Login(){
                 <img src="https://media.discordapp.net/attachments/1095482147156000778/1098024242718707722/image_1.png" alt="" />
                 <h1><b>SUA CONTA PARA TUDO NA REP</b></h1>
             </div>
+            <React.Fragment>
+              {user.showMsg && (
+                <div className="error-container">
+                  <div className="error-message">{user.errorMsg}</div>
+                </div>
+              )}
+            </React.Fragment>
             <form className="login-form">
                 <input 
                     type="email" placeholder="E-MAIL" required name="email" onChange={(e) => setEmail(e.target.value)}
