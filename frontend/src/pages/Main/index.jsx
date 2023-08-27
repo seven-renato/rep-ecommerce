2cff113429ec57ca55212c515f765030c46c9f99
seven-renato <seven.renato@hotmail.com>
2023-08-27 20:35:19 -0300

    slider newsletter navbar componentes

---
+++ frontend/src/pages/Main/index.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import Announcement from "../components/Home/announcement";
+import Navbar from "../../components/Home/navbar";
+// import Slider from "../../components/Home/slider";
+// import Categories from "../components/Home/categories";
+import Newsletter from "../../components/Home/newsletter";
+// import Footer from "../components/Home/footer";
+
+const Home = () => {
+  return (
+    <div>
+      {/* <Announcement /> */}
+      <Navbar />
+      {/* <Categories /> */}
+      {/* <Slider /> */}
+      <Newsletter />
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default Home;
